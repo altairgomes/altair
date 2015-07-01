8b51a10ccb98cc886a43ea8fcac036121beee388
Altair <altair@Excelsior.(none)>
2015-07-01 20:40:31 -0300

    Ajuste e grafico feitos, bom para DEC

--- offset_irregular/Carme/2015/teste.py
+++ offset_irregular/Carme/2015/teste.py
@@ -8,43 +8,59 @@ import scipy.odr.odrpack as odrpack
 y = np.loadtxt('Carme_ephem.dat', skiprows=3, usecols=(2, 20, 21, 37, 16, 36), unpack=True) ## 2: JD; 20: dist_prim X; 21: dist_prim Y; 37: anom. verd.; 16: distancia; 36: anom. med.
 z = np.loadtxt('Carme_total', usecols=(0, 1, 2, 3), unpack=True) ## 0: off RA; 1: off DEC; 2: off_err RA; 3: off_err DEC
 
+eph = np.loadtxt('Carme.eph', skiprows=3, usecols=(2, 35, 16, 34), unpack=True) ## 2: JD; 37: anom. verd.; 16: distancia; 36: anom. med.
+
 k = np.arange(361)
+
+m = np.append(eph[1][0], eph[1])[0:-1]
+n = eph[1]-m
+j = np.where(n < -100.0)
+
+r = []
+for i in np.arange(1995,2016,1):
+    r.append(Time('{}-01-01 00:00:00'.format(i), format='iso').jd - 2451544.5)
+
+print r
+r = np.array(r)
 
 ############## Declinacao ############################################
 
-A = np.vstack([y[0] - 2451544.5, np.sin(y[5]*u.deg)*np.cos(y[5]*u.deg), np.sin(y[5]*u.deg), np.cos(y[5]*u.deg), np.ones(len(y[3]))]).T
+A = np.vstack([y[0] - 2451544.5, np.sin(y[3]*u.deg), np.cos(y[3]*u.deg), np.ones(len(y[3]))]).T
 
-#p = np.linalg.lstsq(A, z[1])
+p = np.linalg.lstsq(A, z[1])
 
 def f(B, x):
     return B[0]*(x[0] - 2451544.5) + B[1]*np.sin(x[1]*u.deg) + B[2]*np.cos(x[1]*u.deg) + B[3]
 
-x = np.vstack([y[0], y[5]])
+x = np.vstack([y[0], y[3]])
 
 linearde = odrpack.Model(f)
 #mydata = odrpack.Data(x, z[1], wd=1./np.power(z[2],2), we=1./np.power(sy,2))
-mydata = odrpack.RealData(x, z[1], sy=z[3])
-myodr = odrpack.ODR(mydata, linearde, beta0=[1.78854119e-03, -1.21822344e+00, 9.88555083e+00, -6.08946292e+00])
-myoutput = myodr.run()
+mydatade = odrpack.RealData(x, z[1], sy=z[3])
+myodrde = odrpack.ODR(mydatade, linearde, beta0=p[0])
+myodrde.set_job(fit_type=2)
+myoutputde = myodrde.run()
 print 'Declinacao\n'
-myoutput.pprint()
+myoutputde.pprint()
 
 #print p[0]
 #print "Desvio padrao DEC:", np.sqrt(((z[1]/z[3] - (A*p[0]).sum(axis=1))**2).sum())
 
-plt.errorbar(y[3], z[1], yerr=z[3], fmt='s', label='Offsets')
+plt.errorbar(y[0] - 2451544.5, z[1], yerr=z[3], fmt='s', label='Offsets')
 #plt.plot(y[3], z[1], 's', label='Offsets')
-for i in np.arange(2000,2010,1):
-    plt.plot(f(myodr.output.beta, np.vstack([Time('{}-01-01 00:00:00'.format(i), format='iso').jd*np.ones(len(k)),k])), label=i)
+plt.plot(eph[0] - 2451544.5, f(myodrde.output.beta, np.vstack([eph[0], eph[1]])), label='Ajuste1')
+#plt.plot(eph[0] - 2451544.5, f(p[0], np.vstack([eph[0], eph[1]])), label='Ajuste2')
 #plt.title('DEC = {:.2f}*sen(A.V.) + {:.2f}*cos(A.V.) + {:.2f}'.format(p[0][0], p[0][1], p[0][2]))
-plt.xlim(0,360)
+#plt.xlim(0,360)
+plt.vlines(eph[0][j] - 2451544.5, -500, 500)
 plt.ylim(-500,500)
-plt.xlabel('Anomalia Verdadeira')
+plt.xlabel('Tempo')
+plt.xticks(r, ['{}'.format(i) for i in np.arange(1995,2016,1)])
 plt.ylabel('Offset (mas)')
 plt.legend()
-plt.axhline(0, color='red')
+plt.axhline(0, color='black')
 fig =plt.gcf()
-fig.set_size_inches(15.0,7.5)
+fig.set_size_inches(20.0,8.0)
 fig.savefig('DEC.png',dpi=100, bbox_inches='tight')
 #plt.savefig('DEC.png')
 plt.clf()
@@ -52,39 +68,42 @@ plt.clf()
 
 #################### Ascensao Reta ##################################
 
-B = np.vstack([y[0] - 2451544.5, np.sin(y[5]*u.deg)*np.cos(y[5]*u.deg), np.sin(y[5]*u.deg), np.cos(y[5]*u.deg), np.ones(len(y[3]))]).T
+B = np.vstack([y[0] - 2451544.5, np.sin(y[3]*u.deg)*np.cos(y[3]*u.deg), np.sin(y[3]*u.deg), np.cos(y[3]*u.deg), np.ones(len(y[3]))]).T
 
-#q = np.linalg.lstsq(B, z[0])
+q = np.linalg.lstsq(B, z[0])
 
 def g(B, x):
     return B[0]*(x[0] - 2451544.5) + B[1]*np.sin(x[1]*u.deg)*np.cos(x[1]*u.deg) + B[2]*np.sin(x[1]*u.deg) + B[3]*np.cos(x[1]*u.deg) + B[4]
 
-x = np.vstack([y[0], y[5]])
+x = np.vstack([y[0], y[3]])
 
 linearra = odrpack.Model(g)
 #mydata = odrpack.Data(x, z[1], wd=1./np.power(z[2],2), we=1./np.power(sy,2))
-mydata = odrpack.RealData(x, z[0], sy=z[2])
-myodr = odrpack.ODR(mydata, linearra, beta0=[3.62659327e-04,7.82345630e+00, -6.40102501e-01, -2.83655752e-01, -9.32980254e-02])
-myoutput = myodr.run()
+mydatara = odrpack.RealData(x, z[0], sy=z[2])
+myodrra = odrpack.ODR(mydatara, linearra, beta0=q[0])
+myodrra.set_job(fit_type=2)
+myoutputra = myodrra.run()
 print '\n\nAscensao Reta\n'
-myoutput.pprint()
+myoutputra.pprint()
 
 #print q[0]
 #print "Desvio padrao RA:", np.sqrt(((z[0]/z[2] - (B*q[0]).sum(axis=1))**2).sum())
 
-plt.errorbar(y[3], z[0], yerr=z[2], fmt='s', label='Offsets')
+plt.errorbar(y[0] - 2451544.5, z[0], yerr=z[2], fmt='s', label='Offsets')
 #plt.plot(y[3], z[0], 's', label='Offsets')
-for i in np.arange(2000,2010,1):
-    plt.plot(g(myodr.output.beta, np.vstack([Time('{}-01-01 00:00:00'.format(i), format='iso').jd*np.ones(len(k)),k])), label=i)
+plt.plot(eph[0] - 2451544.5, g(myodrra.output.beta, np.vstack([eph[0], eph[1]])), label='Ajuste1')
+#plt.plot(eph[0] - 2451544.5, g(q[0], np.vstack([eph[0], eph[1]])), label='Ajuste2')
 #plt.title('RA = {:.2f}*sen^2(A.V.) + {:.2f}*cos^2(A.V.) + {:.2f}*sen(A.V.)*cos(A.V.) + {:.2f}*sen(A.V.) + {:.2f}*cos(A.V.) + {:.2f}'.format(q[0][0], q[0][1], q[0][2], q[0][3], q[0][4], q[0][5]))
-plt.xlim(0,360)
+#plt.xlim(0,360)
+plt.vlines(eph[0][j] - 2451544.5, -500, 500)
 plt.ylim(-500,500)
-plt.xlabel('Anomalia Verdadeira')
+plt.xlabel('Tempo')
+plt.xticks(r, ['{}'.format(i) for i in np.arange(1995,2016,1)])
 plt.ylabel('Offset (mas)')
 plt.legend()
-plt.axhline(0, color='red')
+plt.axhline(0, color='black')
 fig = plt.gcf()
-fig.set_size_inches(15.0,7.5)
+fig.set_size_inches(20.0,8.0)
 fig.savefig('RA.png',dpi=100, bbox_inches='tight')
 
 
