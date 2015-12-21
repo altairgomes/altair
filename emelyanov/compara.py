0e595acedb45ee197629c210c1833915814a59e8
Altair <altair@Excelsior.(none)>
2015-12-21 17:44:03 -0200

    .'

--- emelyanov/compara.py
+++ emelyanov/compara.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from observation_3 import read2
 
-a = 'Nereida_Zhang.eph'
+a = 'Nereida_eme.eph'
 b = 'Nereida_jpl.eph'
 
 c = read2(a, time_col=[0,1,2,3,4,5], time_fmt='iso', coord_col=[6,7], skiprows=8)
@@ -13,7 +13,7 @@ c = read2(a, time_col=[0,1,2,3,4,5], time_fmt='iso', coord_col=[6,7], skiprows=8
 d = read2(b, time_col=[2], time_fmt='jd', coord_col=[4,5,6,7,8,9], skiprows=3)
 
 e = read2('Nereid.dat', time_col=[8], time_fmt='jd', coord_col=[0,1,2,3,4,5])
-f = read2('Nereida_LNA.eph', time_col=[0], time_fmt='jd', coord_col=[1,2,3,4,5,6], skiprows=7)
+f = read2('Nereida_LNA.eph', time_col=[0], time_fmt='jd', coord_col=[1,2,3,4,5,6], skiprows=8)
 
 eme_time = Time(c['times'], format='iso', scale='utc')
 eme_coord = SkyCoord(c['coords'].transpose(), unit=(u.hourangle, u.deg))
@@ -38,21 +38,21 @@ for i in np.arange(1992,2019,2):
 
 r = np.array(r)
 
-plt.plot(jpl_time.jd - 2451544.5, dalfa.mas, label='Right Ascencion')
+#plt.plot(jpl_time.jd - 2451544.5, dalfa.mas, label='Right Ascencion')
 plt.plot(jpl_time.jd - 2451544.5, ddelta.mas, label='Declination')
 
 #plt.plot(pos_time.jd - 2451544.5, dalfapos.mas, 'r+')
-#plt.plot(pos_time.jd - 2451544.5, ddeltapos.mas, 'rx')
+plt.plot(pos_time.jd - 2451544.5, ddeltapos.mas, 'rx')
 
 plt.xlim(Time('1992-01-01 00:00:00').jd - 2451544.5, Time('2018-01-01 00:00:00').jd - 2451544.5)
 #plt.ylim(-500,500)
 #plt.title('Laurene - JUP300')
 plt.xlabel('Time')
 plt.xticks(r, ['{}'.format(i) for i in np.arange(1992,2019,2)])
-plt.ylabel('NEP081 - Zhang (mas)')
+plt.ylabel('NEP081 - Emelyanov (mas)')
 plt.legend()
 plt.axhline(0, color='black')
 fig =plt.gcf()
 fig.set_size_inches((40.0*u.cm).to(u.imperial.inch).value,(16.0*u.cm).to(u.imperial.inch).value)
-fig.savefig('JPL-Zhang_Nereida.png',dpi=100, bbox_inches='tight')
+fig.savefig('JPL-EME_Nereida_DEC.png',dpi=100, bbox_inches='tight')
 plt.clf()
