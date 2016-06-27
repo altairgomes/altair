a8ce1ed7b98a574d7e6b03ecc6c17155e61318e9
altairgomes <altairgomesjr@gmail.com>
2016-06-27 19:48:19 -0300

    .

--- Irregulares/Carme/read.py
+++ Irregulares/Carme/read.py
@@ -55,15 +55,19 @@ for i in [0, 1, 2]:
     for j in np.arange(len(difemedec)):
         f.write('{:16.8f} {:+04.0f} {:+04.0f}\n'.format(time[code == b[i]].jd[j], difemera[j].mas, difemedec[j].mas))
     f.close()
-    j, = plt.plot(time[code == b[i]].jd - 2451544.5, difjpldec.mas, '+', color='blue')
-    m, = plt.plot(time[code == b[i]].jd - 2451544.5, difemedec.mas, '.', color='green')
-    l, = plt.plot(time[code == b[i]].jd - 2451544.5, diflaudec.mas, 'x', color='red')
-    
-r = []
-for i in np.arange(1995,2016,3):
-    r.append(Time('{}-01-01 00:00:00'.format(i), format='iso').jd - 2451544.5)
+#    j, = plt.plot(time[code == b[i]].jd - 2451544.5, difjpldec.mas, '+', color='blue')
+#    m, = plt.plot(time[code == b[i]].jd - 2451544.5, difemedec.mas, '.', color='green')
+#    l, = plt.plot(time[code == b[i]].jd - 2451544.5, diflaudec.mas, 'x', color='red')
+    j, = plt.plot(anom, difjpldec.mas, '+', color='blue')
+    m, = plt.plot(anom, difemedec.mas, '.', color='green')
+    l, = plt.plot(anom, diflaudec.mas, 'x', color='red')
 
-r = np.array(r)
+    
+#r = []
+#for i in np.arange(1995,2016,3):
+#    r.append(Time('{}-01-01 00:00:00'.format(i), format='iso').jd - 2451544.5)
+
+#r = np.array(r)
     
 #plt.xlim(0,360)
 plt.xlim(Time('1995-01-01 00:00:00').jd - 2451544.5, Time('2015-01-01 00:00:00').jd - 2451544.5)
@@ -72,11 +76,12 @@ plt.axhline(0, color='black')
 plt.title('{}'.format(obj), fontsize=sizel)
 plt.xlabel('True Anomaly (degrees)', fontsize=sizel)
 plt.ylabel('Offset (mas)', fontsize=sizel)
-#plt.xticks(np.arange(0,370,45))
-plt.xticks(r, ['{}'.format(i) for i in np.arange(1995,2016,3)])
+plt.xticks(np.arange(0,370,45))
+plt.xlim(0,360)
+#plt.xticks(r, ['{}'.format(i) for i in np.arange(1995,2016,3)])
 #ax = plt.gca().add_artist(first_legend)
-plt.legend([l, j, m], ['STE', 'JUP300', 'Eme2008'], numpoints=1, loc=1, labelspacing=0.25, borderpad=0.1, handlelength=0.5, prop={'size':sizel})
+plt.legend([l, j, m], ['STE', 'JUP300', 'Eme2005'], numpoints=1, loc=3, labelspacing=0.25, borderpad=0.1, handlelength=0.5, prop={'size':sizel})
 plt.tick_params(axis='both', which='major', labelsize=sizel)
 fig =plt.gcf()
 fig.set_size_inches((17.6*u.cm).to(u.imperial.inch).value,(9.9*u.cm).to(u.imperial.inch).value)
-fig.savefig('{}_ephemeris_time.eps'.format(obj), format='eps', dpi=300, bbox_inches='tight')
+fig.savefig('{}_ephemeris_2005.eps'.format(obj), format='eps', dpi=300, bbox_inches='tight')
