353004db78adcab13760bf46b8f0a0d2ed089346
altairgomes <altairgomesjr@gmail.com>
2016-07-13 20:11:34 -0300

    .

--- ephem/ephem.py
+++ ephem/ephem.py
@@ -18,19 +18,9 @@ def compute(k, center, target, jd, vel=False):
 
 ##########################################################################################################
 
-kernel = SPK.open('de430.bsp')
-kernelh = SPK.open('jup300.bsp')
-time = Time(2457023.5, format='jd', scale='utc')
-delta_at = 35*u.s
-delta_t_a = 32.184*u.s
-k = float(1.657e-3)*u.s
-eb = float(1.671e-2)
-n = np.array([6.239996e0, 1.99096871e-7])
-t = time - Time('j2000')
-m = n[0] + n[1]*t.sec
-e = m + eb*np.sin(m)
-delta_et =  delta_t_a  + k*np.sin(e) + delta_at
-
+kernel = SPK.open('de435.bsp')
+kernelnep = SPK.open('jup300.bsp')
+time = Time(2458850.499918981, format='jd', scale='utc')
 time = time.tdb
 
 geop, geov = compute(kernel,0,3,time.jd,vel=True) + compute(kernel,3,399,time.jd,vel=True)
@@ -46,12 +36,12 @@ n = 0
 while True:
     n = n + 1
     tempo = time - delt
-    objp = kernel[0,5].compute(tempo.jd) + kernelh[5,506].compute(tempo.jd)[0:3] - g
-    position = objp*u.km
+    objp, objv = compute(kernel,0,5,tempo.jd,vel=True) + compute(kernelnep,5,506,tempo.jd,vel=True)[:,0:3]
+    position = (objp - geop)*u.km
 #    velocity = (objv - geov)*(u.km/u.day)
 #    print "velocity", velocity
-    dist = np.linalg.norm(position)*u.km
-    delt = (dist/const.c).decompose()
+#    dist = np.linalg.norm(position)*u.km
+#    delt = (dist/const.c).decompose()
 #    vemag = np.linalg.norm(velocity)*(u.km/u.day)
 #    beta = (vemag/const.c).decompose()
 #    print "vemag", vemag
@@ -68,16 +58,14 @@ while True:
 #    r = 1.0 + p
 #    print "r", r
 #    pos2 = ((gammai*position+q*velocity)/r).decompose()
+    pos2 = position
 #    print n, '{:1.15e}'.format(pos2[0])
-    if np.all(np.absolute(time - tempo - delt) < 0.000001*u.s):
+    if np.all(np.absolute(time - tempo - delt) < 0.0001*u.s):
         break
 
 
-#coord = SkyCoord(pos2[0], pos2[1], pos2[2], frame='icrs', unit=u.km, representation='cartesian')
-
-coord = SkyCoord(position[0]*u.km - gcrs.cartesian.x*u.km, position[1]*u.km - gcrs.cartesian.y*u.km, position[2]*u.km - gcrs.cartesian.z*u.km, frame='icrs', representation='cartesian')
+coord = SkyCoord(pos2[0], pos2[1], pos2[2], frame='icrs', unit=u.km, representation='cartesian')
 
 print coord.spherical.lon.hms, coord.spherical.lat.dms
-
 
 #coord = SkyCoord(position[0]*u.km - gcrs.cartesian.x, position[1]*u.km - gcrs.cartesian.y, position[2]*u.km - gcrs.cartesian.z, frame='icrs', representation='cartesian')
