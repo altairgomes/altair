9516b6c7f0fdcbe00fe76f08bff5946a598c4991
altairgomes <altairgomesjr@gmail.com>
2016-07-18 19:55:50 -0300

    .

--- ephem/ephem2.py
+++ ephem/ephem2.py
@@ -9,42 +9,48 @@ import os
 #########################################################################################################
 
 ### Defines coordinates for each location
-sites = {  '0': ['Greenwich',        EarthLocation.from_geodetic( +0.00000000000,   +51.47738889,      65.8)],
-         '511': ['Haute Provence',   EarthLocation.from_geodetic( +5.71569444400,   +43.93186111,     633.9)],
-         '586': ['Pic du Midi',      EarthLocation.from_geodetic( +0.14230555600,   +42.93655556,    2890.5)],
-         '809': ['ESO, La Silla',    EarthLocation.from_geodetic( +289.266250000,   -29.25883333,    2345.4)],
-         '874': ['OPD/LNA',          EarthLocation.from_geodetic( +314.417361100,   -22.53550000,    1810.7)],
-         '568': ['Mauna Kea',        EarthLocation.from_geodetic( +204.527805556,   +19.82611111,    4212.4)],
-         '267': ['CFHT',             EarthLocation.from_geodetic( +204.530444444,   +19.82536111,    4147.8)],
-          '33': ['SOAR',             EarthLocation.from_geodetic( +289.269805556,   -30.23802778,    2693.9)],
-          '97': ['Wise Observatory', EarthLocation.from_geodetic( +34.7625000000,   +30.59566667,     904.2)],
-           '1': ['Paranal VLT',      EarthLocation.from_geodetic( +289.597194444,   -24.62541667,    2635.0)],
-          '13': ['Liverpool Tel.',   EarthLocation.from_geodetic( +342.120800000,   +28.76240000,    2387.6)],
-         '493': ['Calar Alto',       EarthLocation.from_geodetic( +357.453700000,   +37.22360000,    2168.0)],
-         '999': ['NARIT THAI',       EarthLocation.from_geodetic( +98.4666666667,   +18.56666667,    2457.0)],
-          '86': ['Sierra Nevada',    EarthLocation.from_geodetic( +356.615305556,   +37.06413889,    2930.4)],
-          '95': ['La Hita',          EarthLocation.from_geodetic( +356.813944444,   +39.56861111,     674.9)],
-          '20': ['La Palma MERCATOR',EarthLocation.from_geodetic( +342.121694444,   +28.76200000,    2356.9)],
-          '84': ['Cerro Tololo-DEC', EarthLocation.from_geodetic( +289.193583333,   -30.16958333,    2202.7)]}
+sites =
+{
+   '0': ['Greenwich',        EarthLocation.from_geodetic( +0.00000000000,   +51.47738889,      65.8)],
+   '1': ['Paranal VLT',      EarthLocation.from_geodetic( +289.597194444,   -24.62541667,    2635.0)],
+  '13': ['Liverpool Tel.',   EarthLocation.from_geodetic( +342.120800000,   +28.76240000,    2387.6)],
+  '20': ['La Palma MERCATOR',EarthLocation.from_geodetic( +342.121694444,   +28.76200000,    2356.9)],
+  '33': ['SOAR',             EarthLocation.from_geodetic( +289.269805556,   -30.23802778,    2693.9)],
+  '86': ['Sierra Nevada',    EarthLocation.from_geodetic( +356.615305556,   +37.06413889,    2930.4)],
+  '84': ['Cerro Tololo-DEC', EarthLocation.from_geodetic( +289.193583333,   -30.16958333,    2202.7)]
+  '95': ['La Hita',          EarthLocation.from_geodetic( +356.813944444,   +39.56861111,     674.9)],
+  '97': ['Wise Observatory', EarthLocation.from_geodetic( +34.7625000000,   +30.59566667,     904.2)],
+ '267': ['CFHT',             EarthLocation.from_geodetic( +204.530444444,   +19.82536111,    4147.8)],
+ '493': ['Calar Alto',       EarthLocation.from_geodetic( +357.453700000,   +37.22360000,    2168.0)],
+ '511': ['Haute Provence',   EarthLocation.from_geodetic( +5.71569444400,   +43.93186111,     633.9)],
+ '568': ['Mauna Kea',        EarthLocation.from_geodetic( +204.527805556,   +19.82611111,    4212.4)],
+ '586': ['Pic du Midi',      EarthLocation.from_geodetic( +0.14230555600,   +42.93655556,    2890.5)],
+ '809': ['ESO, La Silla',    EarthLocation.from_geodetic( +289.266250000,   -29.25883333,    2345.4)],
+ '874': ['OPD/LNA',          EarthLocation.from_geodetic( +314.417361100,   -22.53550000,    1810.7)],
+ '999': ['NARIT THAI',       EarthLocation.from_geodetic( +98.4666666667,   +18.56666667,    2457.0)],
+}
 
 ### Defines type, name and kernel for each object
 ### Types: 's' for satellites, 'p' for planet
-objects = { ### Planets
-            '599': ['p', 'Jupiter', 'jup300'], ## check kernel
-            ### Satellites
-            '506': ['s', 'Himalia', 'jup300'],
-            '507': ['s', 'Elara', 'jup300'],
-            '508': ['s', 'Pasiphae', 'jup300'],
-            '509': ['s', 'Sinope', 'jup300'],
-            '510': ['s', 'Lysithea', 'jup300'],
-            '511': ['s', 'Carme', 'jup300'],
-            '512': ['s', 'Ananke', 'jup300'],
-            '513': ['s', 'Leda', 'jup300'],
-            '607': ['s', 'Hyperion', 'jup300'], ## check kernel
-            '608': ['s', 'Iapetus', 'jup300'], ## check kernel
-            '609': ['s', 'Phoebe', 'sat359'], ## check kernel
-            '802': ['s', 'Nereid', 'nep081']
-            }
+objects =
+{
+### Planets
+    '599': ['p', 'Jupiter', 'jup300'], ## check kernel
+### Satellites
+    '506': ['s', 'Himalia', 'jup300'],
+    '507': ['s', 'Elara', 'jup300'],
+    '508': ['s', 'Pasiphae', 'jup300'],
+    '509': ['s', 'Sinope', 'jup300'],
+    '510': ['s', 'Lysithea', 'jup300'],
+    '511': ['s', 'Carme', 'jup300'],
+    '512': ['s', 'Ananke', 'jup300'],
+    '513': ['s', 'Leda', 'jup300'],
+    '607': ['s', 'Hyperion', 'jup300'], ## check kernel
+    '608': ['s', 'Iapetus', 'jup300'], ## check kernel
+    '609': ['s', 'Phoebe', 'sat359'], ## check kernel
+    '802': ['s', 'Nereid', 'nep081']
+### TNOs e Centauros
+}
 
 #########################################################################################################
 
