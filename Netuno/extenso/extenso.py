819505e8b60d729f6d79388ff36f8aad892e16b2
altairgomes <altairgomesjr@gmail.com>
2016-08-08 19:52:26 -0300

    .

--- Netuno/extenso/extenso.py
+++ Netuno/extenso/extenso.py
@@ -14,12 +14,17 @@ def gaussian(B,x):
     return B[0]*np.exp(-((x[0]-B[1])*(x[0]-B[1]) + (x[1]-B[2])*(x[1]-B[2]))/(2*B[3]*B[3])) + B[4]
 
 def extenso(B,x):
-    def func_integer(mi):
-        np.exp(-(x[0]-B[2]-mi)*(x[0]-B[2]-mi)/2*B[1]*B[1])*(special.erf((x[1]-B[3]+np.sqrt(B[4]*B[4]-mi*mi))/(np.sqrt(2)*B[1]))-special.erf((x[1]-B[3]-np.sqrt(B[4]*B[4]-mi*mi))/(np.sqrt(2)*B[1])))
-    integral = integrate.quad(func_integer,-B[4], B[4])
+    def func_integer(mi, i):
+        return np.exp(-(x[0][i]-B[2]-mi)*(x[0][i]-B[2]-mi)/2*B[1]*B[1])*(special.erf((x[1][i]-B[3]+np.sqrt(B[4]*B[4]-mi*mi))/(np.sqrt(2)*B[1]))-special.erf((x[1][i]-B[3]-np.sqrt(B[4]*B[4]-mi*mi))/(np.sqrt(2)*B[1])))
+    integral = np.array([integrate.quad(func_integer,-B[4], B[4], args=[i])[0] for i in np.arange(len(x[0]))])
     return B[0]*np.sqrt(np.pi/2)*B[1]*integral + B[5]
 #    A, sigma, x0, y0, R
 
+def extenso2(B,x):
+    def func_integer(mi):
+        return np.exp(-(x[0]-B[2]-mi)*(x[0]-B[2]-mi)/2*B[1]*B[1])*(special.erf((x[1]-B[3]+np.sqrt(B[4]*B[4]-mi*mi))/(np.sqrt(2)*B[1]))-special.erf((x[1]-B[3]-np.sqrt(B[4]*B[4]-mi*mi))/(np.sqrt(2)*B[1])))
+    integral = integrate.quad(func_integer,-B[4], B[4])
+    return B[0]*np.sqrt(np.pi/2)*B[1]*integral + B[5]
 
 def least(func, x, y, sy=None, beta0=None, ifixb=None):
     linear = odrpack.Model(func)
@@ -35,7 +40,7 @@ def least(func, x, y, sy=None, beta0=None, ifixb=None):
 
 image = fits.getdata('Netuno_08.21.06_0001.fits')
 a, b = np.indices(image.shape)
-c = np.where(np.sqrt((a-912)**2 + (b-1060)**2))
+c = np.where(np.sqrt((a-912)**2 + (b-1060)**2) < 10)
 d,e = np.meshgrid(np.arange(c[1].min(), c[1].max()), np.arange(c[0].min(), c[0].max()))
 
 #plt.imshow(image[e,d], cmap='gray')
