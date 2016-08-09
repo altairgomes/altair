5129199cfe583c00d35cb516fc71ab13c41eadb6
altairgomes <altairgomesjr@gmail.com>
2016-08-09 19:12:52 -0300

    .

--- Netuno/extenso/extenso.py
+++ Netuno/extenso/extenso.py
@@ -15,10 +15,10 @@ def gaussian(B,x):
 
 def extenso(B,x):
     def func_integer(mi, i):
-        return np.exp(-(x[0][i]-B[2]-mi)*(x[0][i]-B[2]-mi)/2*B[1]*B[1])*(special.erf((x[1][i]-B[3]+np.sqrt(B[4]*B[4]-mi*mi))/(np.sqrt(2)*B[1]))-special.erf((x[1][i]-B[3]-np.sqrt(B[4]*B[4]-mi*mi))/(np.sqrt(2)*B[1])))
+        return np.exp(-(x[0][i]-B[2]-mi)*(x[0][i]-B[2]-mi)/(2*B[1]*B[1]))*(special.erf((x[1][i]-B[3]+np.sqrt(B[4]*B[4]-mi*mi))/(np.sqrt(2)*B[1]))-special.erf((x[1][i]-B[3]-np.sqrt(B[4]*B[4]-mi*mi))/(np.sqrt(2)*B[1])))
     integral = np.array([integrate.quad(func_integer,-B[4], B[4], args=[i])[0] for i in np.arange(len(x[0]))])
     return B[0]*np.sqrt(np.pi/2)*B[1]*integral + B[5]
-#    A, sigma, x0, y0, R
+#    A, sigma, x0, y0, R, c
 
 def extenso2(B,x):
     def func_integer(mi):
@@ -39,7 +39,7 @@ def least(func, x, y, sy=None, beta0=None, ifixb=None):
 ###########################################################################
 
 image = fits.getdata('Netuno_08.21.06_0001.fits')
-a, b = np.indices(image.shape)
+a, b = np.indices(image.shape) + 1
 c = np.where(np.sqrt((a-912)**2 + (b-1060)**2) < 10)
 d,e = np.meshgrid(np.arange(c[1].min(), c[1].max()), np.arange(c[0].min(), c[0].max()))
 
@@ -49,12 +49,15 @@ d,e = np.meshgrid(np.arange(c[1].min(), c[1].max()), np.arange(c[0].min(), c[0].
 
 x = np.array(c)
 y = image[c]
-beta0 = np.array([20000.0,912.0,1060.0,1.9, 100.0])
 
+beta0 = np.array([20000.0,912.0,1060.0,1.9, 100.0])
 aj = least(func=gaussian, x=x, y=y, beta0=beta0)
-#aje = least(func=extenso, x=x, y=y, beta0=beta0)
+
+beta0 = np.array([20000.0,1.9,912.0,1060.0,6.48, 100.0]) # A, sigma, x0, y0, R, c
+aje = least(func=extenso, x=x, y=y, beta0=beta0)
 
 f = np.sqrt((c[0]-aj.beta[1])**2 + (c[1]-aj.beta[2])**2)
 plt.plot(f, y, 'o')
 plt.plot(f, gaussian(aj.beta, x))
 plt.show()
+
